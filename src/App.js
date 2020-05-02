da8e138ed3f94611b0de9d932f459e5ac6a9216a
hasantezcan <hasantezcan77@gmail.com>
2020-05-02 16:31:04 +0300

    Create the app.module.css file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { Cards, Charts, CountryPicker } from './components';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReacDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import App from './App.js';
 
