b44537573b18c0d61f09d558eb1f83a2dbe51edf
hasantezcan <hasantezcan77@gmail.com>
2020-05-02 13:22:47 +0300

    Create react boilerplate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReacDOM from 'react-dom';
+
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
