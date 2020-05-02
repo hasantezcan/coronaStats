f5b7c191aa068d40dd518c1d959c755956309e50
hasantezcan <hasantezcan77@gmail.com>
2020-05-02 18:10:11 +0300

    Return modifedData object from API

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,16 @@ const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
     try {
-        const response = await axios.get(url);
+        const { data } = await axios.get(url);
         
-        return response;
+        const modifiedData = {
+          confirmed: data.confirmed,
+          recovered: data.recovered,
+          deaths: data.deaths,
+          lastUpdate: data.lastUpdate
+        };
+        
+        return modifiedData;
     } catch (error) {
         
     }
