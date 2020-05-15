d8c99bec9f8dafeb6b3eca1ee5a77b46676f37b8
hasantezcan <hasantezcan77@gmail.com>
2020-05-15 19:51:57 +0300

    Add a fanction that fetch the spesific country data

--- src/App.js
+++ src/App.js
@@ -18,20 +18,19 @@ class App extends React.Component {
   }
 
   handleCountryChange = async (country) => {
-    console.log(country);
-    
-    // fetchData
-    // set the state
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
   }
 
   render() {
-    const { data } = this.state
+    const { data, country} = this.state
 
     return (
       <div className={styles.container}>
         <Cards data={data}/>
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart />
+        <Chart data={data} country={country} />
       </div>
     );
   }
--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,20 @@ import axios from 'axios';
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+
+    let changeableUrl = url;
+
+    if (country) {
+        changeableUrl = `${url}/countries/${country}` 
+    }
+
     try {
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
         
         return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
