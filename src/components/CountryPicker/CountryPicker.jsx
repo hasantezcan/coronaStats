7a638622943ba201d20171199e58bb633c09dd96
hasantezcan <hasantezcan77@gmail.com>
2020-05-15 22:42:36 +0300

    Fix country to global issue

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -21,7 +21,7 @@ const CountryPicker = ({ handleCountryChange }) => {
   return(
     <FormControl className={styles.formControl}>
       <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)} >
-        <option value="global"> Global </option>
+        <option value=""> Global </option>
         {fetchedCountries.map((country, index) => 
           <option key={index} value={country}> {country} </option>
         )}
